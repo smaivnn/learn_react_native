e29634b43265242fd9e9837a7133c4b01ea7bba3
smaivnn <janus321@naver.com>
2022-09-17 15:06:55 +0900

    chapter 2-2 counter

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
 import React, {useState} from 'react';
 
-import {Button, SafeAreaView, Text, View} from 'react-native';
+import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import Greeting from './components/Greeting';
 import Box from './components/Box';
+import Counters from './components/Counters';
 
 const App = () => {
-  const [visible, setVisible] = useState(true);
-  const onPress = () => {
-    setVisible(!visible);
-  };
+  const [count, setCount] = useState(0);
+
+  const onIncrease = () => setCount(count + 1);
+  const onDecrease = () => setCount(count - 1);
 
   return (
-    <SafeAreaView>
-      <Button title="토글" onPress={onPress} />
-
-      {visible && (
-        <View>
-          <Greeting />
-          <Box rounded={true} size="large" color="blue" />
-        </View>
-      )}
+    <SafeAreaView style={styles.full}>
+      <Counters count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+  },
+});
 
 export default App;
